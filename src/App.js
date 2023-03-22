ffec9685e34aea68f3d9330287aa567fd9330347
Klaudia Miłoszewska <klaudiamiloszewska100@gmail.com>
2023-03-22 20:20:46 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import {BsSearch} from 'react-icons/bs';
+import Weather from './components/Weather';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [nav, setNav] = useState(false);
+    const toggleNav = () => {
+        setNav(!nav);
+        if(!nav){
+            document.body.style.overflow = 'hidden'
+        }else{
+            document.body.style.overflow = 'scroll'
+        }
+    }
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const url = `http://api.weatherapi.com/v1/current.json?key=8bce3ea3263944a8b10175644230803&q=${city}&aqi=no`;
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+    });
+    setCity('');
+    setLoading(false);
+  };
+  return (   
+    <div className='w-full min-h-screen py-4 px-2 bg-[url(https://source.unsplash.com/random/?landscape)] bg-cover bg-center'>
+     <header className='flex justify-center mb-2 px-2 w-full'>
+      <form className='flex justify-center w-full rounded-md py-3 px-4 bg-black/70 md:w-3/4'>
+        <input onChange={(e) => setCity(e.target.value)} className='text-white px-4 py-2 w-full bg-transparent border-none outline-none' type="text" placeholder='Search for the city..' />
+        <button onClick={fetchWeather} className='m-2 hover:scale-125 duration-300'><BsSearch className='text-white' size={20}/></button>
+      </form>
+     </header>
+
+     <main className='flex justify-center w-100'>
+      {!weather ? '' : <Weather weather={weather} /> }
+     </main>
     </div>
   );
 }
---
+++ src/components/Weather.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {GiGroupedDrops} from 'react-icons/gi';
+import {BsWind, BsCloudsFill, BsFillSunFill} from 'react-icons/bs';
+
+const Weather =({weather}) => {
+  let uv = weather.current.uv;
+  return (
+    <div className=' text-white w-full font-light flex flex-col items-center justify-center py-4 px-2'>
+
+
+      <div className='flex justify-center items-center flex-col mb-4 w-full rounded-md bg-black/70 lg:flex-row p-6 lg:w-3/4'>
+        <div className='flex flex-col justify-center items-center'>
+        <h3 className='text-3xl my-2 lg:text-5xl'>{weather.location.name}</h3>
+        <h1 className='text-6xl lg:text-8xl'>{weather.current.temp_c}&deg;C</h1>
+        <h4 className='m-2 text-lg lg:text-2xl'>{weather.current.condition.text}</h4>
+        </div>
+        <div>
+        <img className='h-auto w-20 lg:w-56' src={weather.current.condition.icon} alt="Weather Condition" />
+        </div>
+      </div>
+
+
+        <div className='w-full grid grid-cols-2 text-light gap-2 md:gap-4 md:w-3/4'>
+        <div className='flex flex-col justify-center items-center rounded-md bg-black/70 px-4 py-10'>
+          <GiGroupedDrops size={30}/>
+          <h5 className='m-2 font-bold lg:text-xl'>Humidity</h5>
+          <p className='text-md'>{weather.current.humidity}%</p>
+        </div>
+        <div className='flex flex-col justify-center items-center rounded-md bg-black/70 px-4 py-10'>
+          <BsWind size={30}/>
+          <h5 className='m-2 font-bold lg:text-xl'>Wind</h5>
+          <p className='text-md'>{weather.current.wind_kph} km/h</p>
+        </div>
+        <div className='flex flex-col justify-center items-center rounded-md bg-black/70 px-4 py-10'>
+          <BsCloudsFill size={30}/>
+          <h5 className='m-2 font-bold lg:text-xl'>Clouds</h5>
+          <p className='text-md'>{weather.current.wind_kph}%</p>
+        </div>
+        <div className='flex flex-col justify-center items-center rounded-md bg-black/70 px-4 py-10'>
+          <BsFillSunFill size={30}/>
+          <h5 className='m-2 font-bold lg:text-xl'>UV</h5>
+          <p className='text-md'>
+            {uv >= 0 && uv < 3 ? "Low" : uv >= 3 && uv <= 5 ? "Moderate" : uv > 5 && uv <= 7 ? "High" : uv > 7 && uv < 11 ? "Very High" : "Extreme"}
+          </p>
+        </div>
+        </div>
+      </div>
+  )
+}
+
+export default Weather
